97bb3e982e7542759d888247752e7efb1f224b00
Josip Martinovic <jmartinov@pmfst.hr>
2021-03-27 17:24:03 +0100

    Vjezbe2

--- Vjezbe2/Zadatak1.py
+++ Vjezbe2/Zadatak1.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
-    
+import numpy as nps
+x = nps.linspace(0, 2, 100)
 
-
-
-F = float(input("Unesi silu:"))
-m = float(input("Unesi masu:"))
+plt.plot(x, x, label='linear')  # Plot some data on the (implicit) axes.
+plt.plot(x, x**2, label='quadratic')  # etc.
+plt.plot(x, x**3, label='cubic')
+plt.xlabel('x label')
+plt.ylabel('y label')
+plt.title("Simple Plot")
+plt.legend()
