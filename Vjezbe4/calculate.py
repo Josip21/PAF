4a3c48e88f63941c8eb761070fa9403c4ca64987
Josip21 <jmartinov@pmfst.hr>
2022-04-03 21:50:52 +0200

    Datoteka

---
+++ Vjezbe4/calculate.py
@@ -0,0 +1,12 @@
+from re import X
+import modul
+import math
+
+def f1(x):
+    return x*x-2*x
+
+def f2(x):
+    return math.sin(x)
+
+print(modul.value(f1,1))
+print(modul.value(f2,1))
---
+++ Vjezbe4/calculus.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def funkcija(f, x):
+    return (
+
+def funkcija2(f, x, h):
+    
+
+
+
+
+
+
+    
+
+
+    
