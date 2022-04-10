d91670a6be17d563d4b4f924c97e67b6ff48332a
Josip21 <jmartinov@pmfst.hr>
2022-04-10 22:35:35 +0200

    Početak V6

---
+++ Vjezbe6/kukus.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+void pravac(double x1, double x2, double y1, double y2)
+{
+    k = (y2-y1) / (x2-x1)
+    l = -k * (x1) + y1
+    std::cout << " " << std::endl; 
+}
+
+
+int main()
+{
+    pravac(1, 2, 2, 1);
+    
+    return 0;
+
+}
